aedc55899f87042c2a2a4757fc2d799d1d027999
nolangh <nolanghiggins@gmail.com>
2022-02-09 11:03:45 -0700

    -a

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-"use strict";
+/* ------------- const input = Document.getElementById("inpt"); ------------- */
+/* ------------ const output = Document.getElementById("output"); ----------- */
+/* ------------- const button = Document.getElementById("btn"); ------------- */
 
-const myHeader = document.getElementById("header");
+const morning = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const afterNoon = [13, 14, 15, 16];
+const evening = [17, 18, 19];
+const night = [20, 21, 22, 23, 24, 25];
+
+function time(x) {
+  /* ------------------------ timeValue = input.value; ------------------------ */
+  for (const t of morning) {
+    if (x === t) {
+      return "Good Morning";
+    }
+    console.log(time(9));
+  }
+}
