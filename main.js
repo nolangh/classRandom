de9503b08d2d8a054760af13d497a10852049cd1
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-02-17 10:14:26 -0700

    -a

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let cart = (function () {
+/* let cart = (function () {
   let cartItems = []; //product structure { name:"name", price:10.00}
   let cartTotal = 0;
   return {
@@ -21,3 +21,18 @@ let cart = (function () {
     },
   };
 })();
+*/
+
+const list = []; // set an empty array
+
+function add() {
+  const input = document.getElementById("itemInput").value; //this grabs the value from the input
+  console.log(input);
+  list.push(input); // this adds the
+  displayList(); //this calls displayList function
+}
+
+function displayList() {
+  let itemList = ""; //This function loops through the array "list" and displays the li
+  list.forEach((item) => (itemList += `<li>${item}</li>`));
+}
