56ee89c055d411626584f5c892f5797a4adf5dba
nolangh <nolanghiggins@gmail.com>
2022-02-23 12:55:06 -0700

    added phone number regex

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-let cart = (function () {
-  let cartItems = []; //product structure { name:"name", price:10.00}
-  let cartTotal = 0;
-  return {
-    getTotal: function () {
-      return cartTotal;
-    },
-    getItems: function () {
-      return cartItems;
-    },
-    getItemsCount: function () {
-      cartItems.length;
-    },
-    addItem: function (item) {
-      cartItems.push;
-      cartTotal += item.price;
-    },
-    removeItem: function (item) {
-      cartItem = cartItems.filter((anItem) => anItem.name != item.name);
-      carTotal -= item.price;
-    },
-  };
-})();
+const input = document.getElementById("phone");
+const reg = /\(?\d{3}\)?\d{3}\d{4}/;
+
+function phoneNumber() {
+	let result = reg.test(input);
+	if ((result = "true")) {
+		console.log(result);
+		return alert("true");
+	}
+	return alert("false");
+}
