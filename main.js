5343a31f9e93d0fb878b9f235b74285b91864f8f
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-02-23 23:15:32 -0700

    -a

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const input = document.getElementById("phone");
-const reg = /\(?\d{3}\)?\d{3}\d{4}/;
+const reg = /\(?\d{3}\)?\d{3}\d{4}/g;
 
 function phoneNumber() {
 	let result = reg.test(input);
