768c0ab05eadb0020314877fb4a8d3fe22bd93b5
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-02-23 23:25:09 -0700

    -a

---
+++ hackerRank/js/count.js
@@ -0,0 +1,8 @@
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ *
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ * return the number of items in the array where x == y
+ */
+function getCount(objects) {}
