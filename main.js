ead40e705fdd1a08093e0d68224fa8fbc633f847
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-03-02 09:10:28 -0700

    -a

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const input = document.getElementById("phone");
-const reg = /\(?\d{3}\)?\d{3}\d{4}/g;
+let pokemon = getPokemon(displayPokemon);
 
-function phoneNumber() {
-	let result = reg.test(input);
-	if ((result = "true")) {
-		console.log(result);
-		return alert("true");
-	}
-	return alert("false");
+function getPokemon(callBack) {
+	const httpRequest = new XMLHttpRequest();
+	httpRequest.onload = () => {
+		if (
+			httpRequest.readyState == XMLHttpRequest.DONE &&
+			httpRequest.status == 200
+		) {
+			const pokemon = JSON.parse(httpRequest.responseText)?.results;
+			callBack(pokemon);
+			console.log(pokemon);
+		}
+	};
+	httpRequest.open("GET", "https://pokeapi.co/api/v2/pokemon?limit=20");
+	httpRequest.send();
+}
+
+function displayPokemon(pokemon) {
+	//
+	let pokemonHtml = " ";
+	pokemon.forEach((pokemon) => {
+		pokemonHtml += `<div>${pokemon.name}</div>`;
+	});
+	document.querySelector("#pokemon").innerHTML = pokemonHtml;
 }
