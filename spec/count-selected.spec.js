5064520f7fca97de1e3ff20d508d417f0d498470
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-03-03 16:32:13 -0700

    -a

--- spec/count-selected.spec.js
+++ spec/count-selected.spec.js
@@ -6,18 +6,3 @@ describe("countSelected", () => {
 		expect(numberSelected).toBe(0);
 	});
 });
-
- 
-describe('parseNumbers', () =>{
-    it('Should Return an empty array if Parameters are not parsable', () =>{
-        const result = parseNUmbers('ty,'0p', 'yu','9L')
-
-        expect(result).toBeDefined();
-        expect(typeOf result).toBe('object');
-        expect(result.length).toBe('0');
-    });
-    it('', () => {
-
-    })
-})
-
--- spec/parse-numbers.spec.js
+++ spec/parse-numbers.spec.js
@@ -1,4 +1,5 @@
 describe("parseNumbers", () => {
+	//Test 1
 	it("Should Return an empty array if Parameters are not parsable", () => {
 		const result = parseNUmbers("ty", "0p", "yu", "9L");
 
@@ -6,5 +7,22 @@ describe("parseNumbers", () => {
 		expect(typeof result).toBe("object");
 		expect(result.length).toBe(0);
 	});
-	it("", () => {});
+	//Test 2
+	it('it should return an array with a 15 if "15" is passed in', () => {
+		const result = parseNumber("15");
+
+		expect(result.length).toBe(1);
+		expect(result[0]).toBe(15);
+	});
+	//Test 3
+	it('it should return an array with a 15, 34 and 12 if "15", "34" and "12" is passed in', () => {
+		const result = parseNumber("15", "34", "12");
+
+		expect(result.length).toBe(3);
+		expect(result[0]).toBe(15);
+		expect(result[1]).toBe(34);
+		expect(result[2]).toBe(12);
+	});
+	//Test 4
+	//Test 5
 });
