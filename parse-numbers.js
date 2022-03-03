b298f359b3fefddf59665a6fa6a0783f98485d68
nolangh <nolanghiggins@gmail.com>
2022-03-03 16:37:39 -0700

    -a

--- parse-numbers.js
+++ parse-numbers.js
@@ -1,11 +1,1 @@
-function parseNmbers(...x) {
-	const parsed = [];
-	x.forEach((x) => {
-		if (!isNaN(x)) {
-			parsed.push(Number(x));
-		}
-	});
-	return parsed;
-}
-
-console.log(parseNmbers("15", "34", "12"));
+function parseNumbers() {}
