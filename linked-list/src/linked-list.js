1983263eb1eeefda4dea561055b6cfdeda79690e
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-03-14 10:12:17 -0600

    -a

--- linked-list/src/linked-list.js
+++ linked-list/src/linked-list.js
@@ -20,6 +20,12 @@ class LinkedList {
 			this.head = new LinkedListNode(element);
 			this.size += 1;
 		} else {
+			if (element.key < this.head.element.key) {
+				const newNode = new LinkedListNode(element);
+				newNode.next = this.head;
+				this.head = newNode;
+				this.size += 1;
+			}
 			let current = this.head;
 			while (current.next != null) {
 				if (current.element.key == element.key) {
@@ -29,8 +35,20 @@ class LinkedList {
 					newNode.next = current.next;
 					current.next = newNode;
 					this.size += 1;
+				} else {
+					current = current.next;
 				}
 			}
+			current.next = new LinkedListNode(element); //try nesting in different spots
+			this.size += 1;
 		}
 	}
+
+	contains(key) {}
+
+	remove() {
+		//
+	}
+
+	toString() {}
 }
